ed48576ecb278ad20ad547750886d1e06cbfa4d0
RiccardoSangermano <riccardosangermano297@gmail.com>
2025-04-01 19:50:45 +0200

    COMMIT

--- Welcome.jsx
+++ Welcome.jsx
@@ -1,14 +1,22 @@
 import Alert from 'react-bootstrap/Alert';
 
-function RedAlert() {
-  return (
-    <Alert variant="danger">
-        <Alert.Heading>Attenzione!</Alert.Heading>
-      This is a danger alert with{' '}
-      <Alert.Link href="#">an example link</Alert.Link>. Give it a click if
-      you like.
-    </Alert>
-  );
+
+function MyAlert() {
+return (
+<Alert variant="success">
+<Alert.Heading>Hey nice to see you</Alert.Heading>
+<p>
+Aww yeah, you successfully read this important alert message. This
+example text is going to run a bit longer so that you can see how
+spacing within an alert works with this kind of content.
+</p>
+<hr />
+<p className="mb-0">
+Whenever you need to, be sure to use margin utilities to keep things
+nice and tidy.
+</p>
+</Alert>
+  )
 }
 
-export default RedAlert;
+export default MyAlert
